b696c09b62018df7cc5ac18583767052c7f64595
Amit Shinde <Amit Shinde>
2016-12-02 16:50:58 +0530

    First Commit

---
+++ src/AspNetCoreBlogPost/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlogPost.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
